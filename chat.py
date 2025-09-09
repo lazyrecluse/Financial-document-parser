3617b356599a9de67a1cf42cee190d52355db454
lazyrecluse <nabiyyrhama@gmail.com>
2025-09-08 15:41:21 -1200

    updated chat persistence

--- chat.py
+++ chat.py
@@ -1,7 +1,14 @@
 from llama_index.core.storage.chat_store import SimpleChatStore
 from llama_index.core.memory import ChatMemoryBuffer
+import os
 
-chat_store = SimpleChatStore()
+persist_path =  "chat_history.json"
+
+if os.path.exists(persist_path):
+    chat_store= SimpleChatStore.from_persist_path(persist_path)
+    
+else:
+    chat_store = SimpleChatStore()
 
 chat_memory = ChatMemoryBuffer.from_defaults(
     token_limit=3000,
