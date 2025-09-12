edabb9f535336b4b5ca6ac0b5118d4dad0d13810
lazyrecluse <nabiyyrhama@gmail.com>
2025-09-12 05:33:46 -1200

    correct implementation of workflow

--- agent.py
+++ agent.py
@@ -1,21 +1,25 @@
 from llama_index.core.node_parser import SemanticSplitterNodeParser
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.core import (SimpleDirectoryReader, VectorStoreIndex, 
-                              StorageContext
+                              StorageContext, load_index_from_storage
                             )
-from google import genai
+from pydantic import BaseModel, ConfigDict
 from llama_index.core.llms import ChatMessage
-# from llama_index.core.extractors import  (SummaryExtractor,
-#     QuestionsAnsweredExtractor, KeywordExtractor
-#     )
+from chat import chat_memory
 from config import embed_model
 from workflows import Workflow, step
 from workflows.events import Event, StartEvent, StopEvent
 
-#Event to be emitted for streamlit to consume
+#User defined type to hold index returned after parsing
+class ParsedDocument(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)#so pydantic ignores storage
+                                                           #context's complex dtype
+    storage_context: StorageContext
+    
+#Event to be emitted for streamlit to consume   
 class ParsedEvent(Event):
     success_message: str
-    
+    data: ParsedDocument
 #workflow defining how event is generated for storageflow to trigger
 class ParseFlow(Workflow):
     #parses document and triggers event start
@@ -27,13 +31,27 @@ class ParseFlow(Workflow):
         index = VectorStoreIndex.from_documents(document,
             transformations=[SemanticSplitterNodeParser(
             buffer_size=3, breakpoint_percentile_threshold=95, embed_model=embed_model),
-
-        ])
+        
+        ] )
         index.storage_context.persist(persist_dir="storage")
-        return ParsedEvent('Document parsed and stored successfully')
+        data = ParsedDocument(storage_context=index.storage_context)
+        success_message = "Document parsed and stored successfully"
+        return ParsedEvent(success_message=success_message, data=data)
+    
     @step
-    async def StopFlow(self, ev: Event) -> StopEvent:
-        return StopEvent()
+    async def create_engine(self, ev: ParsedEvent) -> StopEvent:
+        parsed_data = ev.data #access data written to parsed event
+        storage_context = parsed_data.storage_context #accesss storage context within data
+        index = load_index_from_storage(storage_context, embed_model= embed_model)
+        chat_engine = index.as_chat_engine(
+    chat_mode="context",
+    memory=chat_memory,
+    system_prompt=(
+        "You are a chatbot, able to parse financial documents and offer crucial"
+        "to your users. Ensure you do your best in offering your aid"
+    ),
+)
+        return StopEvent(result = chat_engine)  
 
 async def main():
     w = ParseFlow(timeout=60, verbose=False)
